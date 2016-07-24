6f4560efd849e8cb96b733862c10ba2a1e5ec012
Aman Sardana <aman.sardana83@gmail.com>
2016-07-24 13:40:08 -0500

    Master - Update 4

--- mergetest/src/main/java/com/example/MyTest.java
+++ mergetest/src/main/java/com/example/MyTest.java
@@ -5,7 +5,7 @@ public class MyTest {
 	public void testMerge(){
 
 
-		System.out.println("First line - update 3");
+		System.out.println("First line - update 4");
 		System.out.println("Second line");
 
 	}
