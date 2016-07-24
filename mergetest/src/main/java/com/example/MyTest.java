480b01763df8562dedd5b927ddbe8c00b170e32c
Aman Sardana <aman.sardana83@gmail.com>
2016-07-24 13:42:46 -0500

    development - update 5

--- mergetest/src/main/java/com/example/MyTest.java
+++ mergetest/src/main/java/com/example/MyTest.java
@@ -5,7 +5,7 @@ public class MyTest {
 	public void testMerge(){
 
 
-		System.out.println("First line - update 3");
+		System.out.println("First line - update 5");
 		System.out.println("Second line");
 
 	}
